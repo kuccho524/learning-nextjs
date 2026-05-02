cb998b9cdb32f83ae014fde65d35e273572831df
kuccho524 <kucchachachanocha@gmail.com>
2026-05-02 16:07:56 +0900

    learn Server Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,34 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// type Post = {
+//   id: number;
+//   title: string
+// }
 
-export default function Home() {
-  return <h1>Hello World</h1>;
+import { Suspense } from "react";
+import SlowComponent from "./SlowComponent";
+
+// export default async function Home() {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const posts: Post[] = await res.json();
+
+//   return (
+//     <div>
+//       <h1>記事一覧</h1>
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.id}>{post.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+export default async function Home() {
+  return (
+    <div>
+      <h1>メインコンテンツ(すぐに表示)</h1>
+      {/* <Suspense fallback={<div>思い込んポーンネントを読み込み中・・・</div>}> */}
+      <SlowComponent />
+      {/* </Suspense> */}
+    </div>
+  );
 }
